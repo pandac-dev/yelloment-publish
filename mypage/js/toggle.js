42a29bf768a98b4e2951d6b1f5e8dccf7c73795d
pandac-jm <jmpark@pandac.co.kr>
2021-02-24 15:58:02 +0900

    commit

--- mypage/js/toggle.js
+++ mypage/js/toggle.js
@@ -1,14 +1,19 @@
-const subToggle = document.querySelector(".subscribe_toggle");
-const subToggleTxt = document.querySelector('.subtoggletxt');
-subToggle.addEventListener('click',function(){
-    if(subToggle.classList.contains('active')){
-        subToggleTxt.textContent = "구독 해지";
-        subToggleTxt.style.color='#bdc1c8';
-        subToggle.classList.remove('active');
+// const subToggle = document.querySelector(".subscribe_toggle");
+// const subToggleTxt = document.querySelector('.subtoggletxt');
+// subToggle.addEventListener('click',function(){
+//     if(subToggle.classList.contains('active')){
+//         subToggleTxt.textContent = "구독 해지";
+//         subToggleTxt.style.color='#bdc1c8';
+//         subToggle.classList.remove('active');
         
-    }else{
-        subToggleTxt.textContent = "구독중";
-        subToggleTxt.style.color='#333';
-        subToggle.classList.add('active');
-    }
+//     }else{
+//         subToggleTxt.textContent = "구독중";
+//         subToggleTxt.style.color='#333';
+//         subToggle.classList.add('active');
+//     }
+// });
+
+const subToggle = document.querySelector(".subscribe_toggle");
+subToggle.addEventListener('click',function(){
+    subToggle.classList.toggle('active');
 });
