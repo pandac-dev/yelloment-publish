0258cdd86bc3583312a4ecae387c8fd3e51024ef
pandac-jm <jmpark@pandac.co.kr>
2021-03-12 15:34:43 +0900

    COMMIT

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ $(function(){
         infinite: true,
         arrows:false,//화살표
         dots:true,//인디케이터
-        autoplay:true,//자동재생
+        autoplay:false,//자동재생
         fade:false,//페이드인 효과
         autoplaySpeed:7000,//재생시간
         pauseOnHover:false,//호버시 멈춤 해제
